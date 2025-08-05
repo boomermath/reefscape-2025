241fd2848ffb92bc7cf6a01f8ccea3653e432255
boomermath <boomermath13@gmail.com>
2025-08-04 23:37:25 -0400

    1

--- ylog/src/main/java/processor/NetworkTableProcessor.java
+++ ylog/src/main/java/processor/NetworkTableProcessor.java
@@ -1,5 +1,69 @@
 package processor;
 
+import edu.wpi.first.networktables.GenericPublisher;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.networktables.StructPublisher;
+import edu.wpi.first.util.struct.Struct;
+import edu.wpi.first.util.struct.StructSerializable;
+import entry.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class NetworkTableProcessor implements YLogProcessor {
-    
+    private final Map<String, GenericPublisher> publishers = new HashMap<>();
+
+
+    @Override
+    public void process(NumberYLogEntry entry) {
+        NetworkTableInstance inst = NetworkTableInstance.getDefault().addSchema();
+
+        NetworkTableInstance.getDefault()
+                .g
+    }
+
+    @Override
+    public void process(NumberArrayYLogEntry entry) {
+
+    }
+
+    @Override
+    public void process(BooleanYLogEntry entry) {
+
+    }
+
+    @Override
+    public void process(BooleanArrayYLogEntry entry) {
+
+    }
+
+    @Override
+    public void process(DoubleArrayYLogEntry value) {
+
+    }
+
+    @Override
+    public void process(DoubleYLogEntry value) {
+
+    }
+
+    @Override
+    public void process(FloatYLogEntry value) {
+
+    }
+
+    @Override
+    public void process(FloatArrayYLogEntry value) {
+
+    }
+
+    @Override
+    public void process(RawYLogEntry value) {
+
+    }
+
+    @Override
+    public <T extends StructSerializable> void process(StructYLogEntry<T> entry) {
+
+    }
 }
--- ylog/src/main/java/processor/YLogProcessor.java
+++ ylog/src/main/java/processor/YLogProcessor.java
@@ -1,17 +1,17 @@
 package processor;
 
-import edu.wpi.first.util.struct.Struct;
 import edu.wpi.first.util.struct.StructSerializable;
+import entry.*;
 
 public interface YLogProcessor {
-    void process(long value);
-    void process(long[] value);
-    void process(boolean value);
-    void process(boolean[] value);
-    void process(double value);
-    void process(double[] value);
-    void process(float[] value);
-    void process(float value);
-    void process(byte[] value);
-   <T extends StructSerializable> void process(Struct<T> struct, T value);
+    void process(NumberYLogEntry entry);
+    void process(NumberArrayYLogEntry entry);
+    void process(BooleanYLogEntry entry);
+    void process(BooleanArrayYLogEntry entry);
+    void process(DoubleArrayYLogEntry value);
+    void process(DoubleYLogEntry value);
+    void process(FloatYLogEntry value);
+    void process(FloatArrayYLogEntry value);
+    void process(RawYLogEntry value);
+   <T extends StructSerializable> void process(StructYLogEntry<T> entry);
 }
